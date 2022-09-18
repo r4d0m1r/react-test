c99421b32f1d4fb1e4ab42ccafce6fddf9f442a9
high1 <high1@mailfence.com>
2022-09-18 18:54:11 +0200

    pref: used signals from preact
    - used signals from preact to avoid the need for separating ClockHands
    into a separate component, and using signals only hands are rerended

--- src/Hand.tsx
+++ src/Hand.tsx
@@ -1,24 +1,26 @@
+import type { ReadonlySignal } from '@preact/signals-react';
 import type { FunctionComponent, SVGProps } from 'react';
 
 type HandProps = {
   length: number;
   limit?: number;
   stationary?: boolean;
-} & SVGProps<SVGLineElement>;
+  transform: ReadonlySignal<string>;
+} & Omit<SVGProps<SVGLineElement>, 'transform'>;
 
 export const Hand: FunctionComponent<HandProps> = ({
   className = '',
   length = 0,
   limit = 94,
   stationary,
+  transform,
   ...rest
-}) => {
-  return (
-    <line
-      className={`stroke-cap-round ${className}`}
-      y1={stationary ? length - limit : undefined}
-      y2={-(stationary ? limit : length)}
-      {...rest}
-    />
-  );
-};
+}) => (
+  <line
+    className={`stroke-cap-round ${className}`}
+    transform={transform.value}
+    y1={stationary ? length - limit : undefined}
+    y2={-(stationary ? limit : length)}
+    {...rest}
+  />
+);
