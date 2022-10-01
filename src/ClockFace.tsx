f6bf2c7b0dc5b8a0f96f06b65ef0b506e96c89ff
high1 <high1@users.noreply.github.com>
2022-10-01 14:32:21 +0200

    perf: preact signals (#22)
    - avoid unnecessary rerendering by using preact signals
    - moved everything to ClockFace and ClockHand - signals remove the need
    for granularly setting the state for clock hands only
    - updated libraries so that the production build works also

--- src/ClockFace.tsx
+++ src/ClockFace.tsx
@@ -1,34 +1,73 @@
-import { ClockHands, rotate } from 'ClockHands';
-import { Hand } from 'Hand';
+import { useComputed, useSignal, useSignalEffect } from '@preact/signals-react';
+import { ClockHand } from 'ClockHand';
 import type { FunctionComponent } from 'react';
 
-export const ClockFace: FunctionComponent = () => (
-  <div className="flex items-center justify-center h-full @dark:bg-neutral-700">
-    <svg viewBox="0 0 200 200" className="h-95vmin">
-      <g className="translate-100px">
-        <circle
-          className="stroke-neutral-900 @dark:stroke-neutral-100 fill-none"
-          r="99"
-        />
-        {Array.from({ length: 60 }, (_, index) => ({
-          isHour: index % 5 === 0,
-        })).map(({ isHour }, index, { length }) => (
-          <Hand
-            key={index}
-            transform={rotate(index / length, 0)}
-            className={
-              isHour
-                ? 'stroke-neutral-800 @dark:stroke-neutral-200 stroke-width-2'
-                : 'stroke-neutral-400 @dark:stroke-neutral-600'
-            }
-            length={isHour ? 7 : 3}
-            stationary
+const getSecondsSinceMidnight = (): number =>
+  (Date.now() - new Date().setHours(0, 0, 0, 0)) / 1000;
+
+const rotate = (rotate: number, fractionDigits = 1) =>
+  `rotate(${(rotate * 360).toFixed(fractionDigits)})`;
+
+export const ClockFace: FunctionComponent = () => {
+  const time = useSignal(getSecondsSinceMidnight());
+  const miliseconds = useComputed(() => rotate(time.value % 1, 0));
+  const seconds = useComputed(() => rotate((time.value % 60) / 60));
+  const minutes = useComputed(() => rotate(((time.value / 60) % 60) / 60));
+  const hours = useComputed(() => rotate(((time.value / 60 / 60) % 12) / 12));
+  useSignalEffect(() => {
+    let frame = requestAnimationFrame(function loop() {
+      time.value = getSecondsSinceMidnight();
+      frame = requestAnimationFrame(loop);
+    });
+    return () => cancelAnimationFrame(frame);
+  });
+  return (
+    <div className="flex items-center justify-center h-full @dark:bg-neutral-700">
+      <svg viewBox="0 0 200 200" className="h-95vmin">
+        <g className="translate-100px">
+          <circle
+            className="stroke-neutral-900 @dark:stroke-neutral-100 fill-none"
+            r="99"
           />
-        ))}
-      </g>
-      <g className="translate-100px">
-        <ClockHands />
-      </g>
-    </svg>
-  </div>
-);
+          {Array.from({ length: 60 }, (_, index) => ({
+            isHour: index % 5 === 0,
+          })).map(({ isHour }, index, { length }) => (
+            <ClockHand
+              key={index}
+              transform={{ value: rotate(index / length, 0) }}
+              className={
+                isHour
+                  ? 'stroke-neutral-800 @dark:stroke-neutral-200 stroke-width-2'
+                  : 'stroke-neutral-400 @dark:stroke-neutral-600'
+              }
+              length={isHour ? 7 : 3}
+              stationary
+            />
+          ))}
+        </g>
+        <g className="translate-100px">
+          <ClockHand
+            transform={miliseconds}
+            className="stroke-neutral-200 @dark:stroke-neutral-600 stroke-width-5 will-change-transform"
+            length={83}
+          />
+          <ClockHand
+            transform={hours}
+            className="stroke-neutral-800 @dark:stroke-neutral-200 stroke-width-4"
+            length={50}
+          />
+          <ClockHand
+            transform={minutes}
+            className="stroke-neutral-800 @dark:stroke-neutral-200 stroke-width-3"
+            length={70}
+          />
+          <ClockHand
+            transform={seconds}
+            className="stroke-red-500 stroke-width-2"
+            length={77}
+          />
+        </g>
+      </svg>
+    </div>
+  );
+};
--- src/ClockHand.tsx
+++ src/ClockHand.tsx
@@ -1,22 +1,29 @@
 import type { FunctionComponent, SVGProps } from 'react';
 
-type HandProps = {
+type SignalLike<T> = {
+  value: T;
+};
+
+type ClockHandProps = {
   length: number;
   limit?: number;
   stationary?: boolean;
-} & SVGProps<SVGLineElement>;
+  transform: SignalLike<string>;
+} & Omit<SVGProps<SVGLineElement>, 'transform'>;
 
-export const Hand: FunctionComponent<HandProps> = ({
+export const ClockHand: FunctionComponent<ClockHandProps> = ({
   className = '',
   length = 0,
   limit = 94,
   stationary,
+  transform: { value },
   ...rest
 }) => (
   <line
     className={`stroke-cap-round ${className}`}
     y1={stationary ? length - limit : undefined}
     y2={-(stationary ? limit : length)}
+    transform={value}
     {...rest}
   />
 );
