07cf7a7c1654af52fbc5621ee25e92931867ceef
high1 <high1@users.noreply.github.com>
2023-01-03 10:57:38 +0100

    chore: configuration updates (#56)
    - use env variable for base
    - renamed env file
    - stricter tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  base: '/react-clock/',
-  build: { commonjsOptions: { include: [] } },
-  optimizeDeps: { disabled: false },
-  plugins: [react(), uno(), tsconfigPaths()],
-});
+export default ({ mode }: ConfigEnv) =>
+  defineConfig({
+    base: loadEnv(mode, process.cwd(), '')['BASE'] ?? '',
+    build: { commonjsOptions: { include: [] } },
+    optimizeDeps: { disabled: false },
+    plugins: [react(), uno(), tsconfigPaths()],
+  });
